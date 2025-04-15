3f14c982ba1d0f05bde53e0c83391b6f05c39289
Arold Daryl Tchouala <AroldDaryl.TchoualaMboumanah@edu.sait.ca>
2025-04-15 09:59:44 -0600

    Update
    - Added weather API
    - Added breakpoints

--- script.js
+++ script.js
@@ -14,3 +14,18 @@ document.querySelectorAll('.carousel').forEach(carousel =>{
 });
 
 alert("Hello, please hover any image to stop the automatic slide show. Thanks!");
+
+
+
+
+
+
+
+  fetch('https://api.open-meteo.com/v1/forecast?latitude=21.17&longitude=-86.85&current_weather=true')
+  .then(res => res.json())
+  .then(data => {
+     const weatherDiv = document.querySelector('#weather');
+     const temp= data.current_weather.temperature;
+     weatherDiv.textContent = `Cacun:  ${temp}°C`;
+
+  });
