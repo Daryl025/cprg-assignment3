41497ba97ed9ebeb5fa717d009409ed5939ad54b
Arold Daryl Tchouala <AroldDaryl.TchoualaMboumanah@edu.sait.ca>
2026-03-17 16:57:04 -0600

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,13 @@ document.querySelectorAll('.carousel').forEach(carousel =>{
     carousel.addEventListener('mouseenter', () =>  {
         carousels.forEach(track => track.style.animationPlayState='paused');
     });
+    carousel.addEventListener('mouseleave', () => {
+        carousels.forEach(track => track.style.animationPlayState='running');
+    });
+});
+
+alert("Hello, please hover any image to stop the automatic slide show. Thanks!");
+
 
 
 
